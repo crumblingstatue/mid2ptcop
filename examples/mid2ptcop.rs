99e521a06231f1a3e6eff745a6e2fc104af4d2ff
crumblingstatue <radiantstatue@gmail.com>
2025-12-26 17:21:17 +0100

    Initial commit

---
+++ examples/mid2ptcop.rs
@@ -0,0 +1,177 @@
+use {
+    clap::Parser,
+    mid2ptcop::write_midi_to_pxtone,
+    ptcow::{
+        Bps, ChNum, EnvelopeSrc, EventPayload, Herd, MooInstructions, PcmData, Song, Voice,
+        VoiceFlags, VoiceInstance, VoiceUnit,
+    },
+    soundfont::{
+        SoundFont2,
+        raw::{RawSoundFontData, SampleChunk},
+    },
+    std::{
+        fs::File,
+        io::{Read, Seek},
+    },
+};
+
+#[derive(clap::Parser)]
+struct Args {
+    midi_path: String,
+    /// Path to a .sf2 soundfont to load voices from
+    #[arg(short = 'f', long = "soundfont")]
+    sf_path: Option<String>,
+    out_path: String,
+}
+
+fn soundfont_stuff(sf_path: &str, ins: &mut MooInstructions, used_programs: &[u16]) {
+    let mut file = File::open(sf_path).unwrap();
+    let data = RawSoundFontData::load(&mut file).unwrap();
+    let sf2 = SoundFont2::from_raw(data);
+    let samp_chk = sf2.sample_data.smpl.unwrap();
+    for &prog in used_programs {
+        add_soundfont(ins, prog, &sf2, &mut file, &samp_chk);
+    }
+    assert_eq!(ins.voices.len(), used_programs.len());
+}
+
+fn add_soundfont(
+    ins: &mut MooInstructions,
+    prog: u16,
+    sf2: &SoundFont2,
+    file: &mut File,
+    samp_chk: &SampleChunk,
+) {
+    let p = sf2
+        .presets
+        .iter()
+        .find(|p| p.header.preset == prog)
+        .unwrap();
+    let mut rate = 0;
+    let mut sample_data = Vec::new();
+    let mut basic_key: u16 = 0;
+    p.zones
+        .iter()
+        .filter_map(|z| {
+            z.instrument().map(|id| {
+                let instrument = &sf2.instruments[*id as usize];
+
+                for (i, z) in instrument.zones.iter().enumerate() {
+                    if let Some(sample_id) = z.sample() {
+                        let samp_head = &sf2.sample_headers[*sample_id as usize];
+                        rate = samp_head.sample_rate;
+                        if i > 1 {
+                            break;
+                        }
+                        match samp_head.sample_type {
+                            soundfont::raw::SampleLink::None => todo!(),
+                            soundfont::raw::SampleLink::MonoSample => {}
+                            soundfont::raw::SampleLink::RightSample => todo!(),
+                            soundfont::raw::SampleLink::LeftSample => todo!(),
+                            soundfont::raw::SampleLink::LinkedSample => todo!(),
+                            soundfont::raw::SampleLink::RomMonoSample => todo!(),
+                            soundfont::raw::SampleLink::RomRightSample => todo!(),
+                            soundfont::raw::SampleLink::RomLeftSample => todo!(),
+                            soundfont::raw::SampleLink::RomLinkedSample => todo!(),
+                            soundfont::raw::SampleLink::VorbisMonoSample => todo!(),
+                            soundfont::raw::SampleLink::VorbisRightSample => todo!(),
+                            soundfont::raw::SampleLink::VorbisLeftSample => todo!(),
+                            soundfont::raw::SampleLink::VorbisLinkedSample => todo!(),
+                        }
+
+                        let start = (samp_head.start as u64 * 2) + samp_chk.offset;
+                        let end = (samp_head.end as u64 * 2) + samp_chk.offset;
+                        let len: usize = (end - start) as usize;
+                        if len > 16_000_000 {
+                            panic!("Too large sample data");
+                        }
+                        file.seek(std::io::SeekFrom::Start(start)).unwrap();
+                        let mut buf = vec![0; len];
+                        file.read_exact(&mut buf).unwrap();
+                        sample_data.extend_from_slice(&buf);
+                        let base_key = samp_head.origpitch;
+                        let pitch_adj = samp_head.pitchadj;
+                        let freq = (base_key as u32 * 256).strict_add_signed(pitch_adj as i32 * 26);
+                        dbg!(freq);
+                        // TODO: This is in ptcow source code
+                        const DEFAULT_KEY: u32 = 24576;
+                        basic_key = (DEFAULT_KEY - freq) as u16;
+                    }
+                }
+            })
+        })
+        .for_each(|()| {});
+    add_soundfont_voice(ins, p.header.name.clone(), sample_data, rate, basic_key);
+}
+
+fn add_soundfont_voice(
+    ins: &mut MooInstructions,
+    name: String,
+    sample_data: Vec<u8>,
+    rate: u32,
+    basic_key: u16,
+) {
+    eprintln!("{name}: {basic_key}");
+    let mut voice = Voice {
+        name,
+        ..Default::default()
+    };
+    if voice.name.len() >= 16 {
+        panic!();
+    }
+    let pcm_data = PcmData {
+        num_samples: sample_data.len() as u32 / 2,
+        sps: rate,
+        ch: ChNum::Mono,
+        bps: Bps::B16,
+        smp: sample_data,
+    };
+
+    let vu = VoiceUnit {
+        basic_key: basic_key as i32,
+        volume: 1,
+        pan: 0,
+        tuning: 1.0,
+        flags: VoiceFlags::SMOOTH,
+        data: ptcow::VoiceData::Pcm(pcm_data),
+        envelope: EnvelopeSrc::default(),
+    };
+    voice.units.push(vu);
+    let vi = VoiceInstance {
+        num_samples: 0,
+        sample_buf: Vec::new(),
+        env: Vec::new(),
+        env_release: 0,
+    };
+    voice.insts.push(vi);
+    ins.voices.push(voice);
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut ins = MooInstructions::new(44_100);
+    let mut song = Song::default();
+    song.fmt.kind = ptcow::FmtKind::Collage;
+    song.fmt.ver = ptcow::FmtVer::V5;
+    let mut herd = Herd::default();
+    let mid_data = std::fs::read(&args.midi_path).unwrap();
+    let out = write_midi_to_pxtone(&mid_data, &mut herd, &mut song, 32);
+    if let Some(sf_path) = &args.sf_path {
+        let mut used_programs: Vec<_> = out.used_programs.iter().collect();
+        used_programs.sort_by_key(|p| *p.1);
+        let used_programs: Vec<u16> = used_programs
+            .into_iter()
+            .map(|p| p.0.as_int() as u16)
+            .collect();
+        soundfont_stuff(sf_path, &mut ins, &used_programs);
+    }
+    // TODO: Enforce this?
+    // Fails to serialize with debug events.
+    // Probably due to mismatched number of events
+    song.events
+        .eves
+        .retain(|eve| !matches!(eve.payload, EventPayload::PtcowDebug(_)));
+    let ptcop = ptcow::serialize_project(&song, &herd, &ins).unwrap();
+    std::fs::write(args.out_path, &ptcop).unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,211 @@
+use {
+    midly::{MetaMessage, MidiMessage, TrackEventKind, num::u7},
+    ptcow::{Event, EventPayload, Herd, Song, Unit, UnitIdx},
+    std::collections::HashMap,
+};
+
+pub type UsedPrograms = HashMap<u7, u32>;
+
+pub struct Output {
+    pub used_programs: UsedPrograms,
+}
+
+fn get_max_tempo(tracks: &[midly::Track]) -> u32 {
+    let mut max = 0;
+    for track in tracks {
+        for ev in track {
+            if let TrackEventKind::Meta(msg) = ev.kind
+                && let MetaMessage::Tempo(u24) = msg
+            {
+                max = max.max(u24.as_int())
+            }
+        }
+    }
+    max
+}
+
+/// Write midi song to pxtone
+pub fn write_midi_to_pxtone(
+    mid_data: &[u8],
+    herd: &mut Herd,
+    song: &mut Song,
+    base_key: u8,
+) -> Output {
+    song.events.eves.clear();
+    herd.units.clear();
+    let mut used_programs: UsedPrograms = HashMap::new();
+    let (header, track_iter) = midly::parse(mid_data).unwrap();
+    // We only support parallel laid out tracks
+    assert!(header.format == midly::Format::Parallel);
+    let tracks = track_iter.collect_tracks().unwrap();
+    let ticks_per_beat = match header.timing {
+        midly::Timing::Metrical(u15) => u15.as_int(),
+        midly::Timing::Timecode(_fps, _) => todo!(),
+    };
+    song.master.timing.ticks_per_beat = ticks_per_beat;
+    let max_tempo = get_max_tempo(&tracks);
+    let mut max_clock = 0;
+    for (track_idx, track) in tracks.iter().enumerate() {
+        let unit = Unit {
+            name: format!("Track {track_idx}-0"),
+            ..Default::default()
+        };
+        if unit.name.len() >= 16 {
+            panic!();
+        }
+        herd.units.push(unit);
+        let mut clock = 0;
+        let mut clock_mul = 1.0;
+        let mut pitch_bend: f64 = 0.0;
+        let mut last_key = None;
+        for (ev_idx, event) in track.iter().enumerate() {
+            match event.kind {
+                TrackEventKind::Midi { message, .. } => match message {
+                    MidiMessage::NoteOff { .. } => {
+                        // We calculate how long notes last in the `NoteOn` event, so we do nothing here
+                    }
+                    MidiMessage::NoteOn { key, vel } => {
+                        last_key = Some(key);
+                        push_key_event(song, base_key, track_idx, clock, pitch_bend, key);
+                        // If velocity is zero, we don't want to emit an `On` event.
+                        if vel == 0 {
+                            //continue;
+                        }
+                        song.events.eves.push(Event {
+                            payload: EventPayload::Velocity(i16::from(vel.as_int())),
+                            unit: UnitIdx(track_idx as u8),
+                            tick: clock,
+                        });
+                        // Find the next note off event for the duration
+                        let mut duration = 'block: {
+                            let mut clock2 = clock;
+                            for ev in track.iter().skip(ev_idx) {
+                                clock2 += ev.delta.as_int();
+                                if let TrackEventKind::Midi {
+                                    channel: _,
+                                    message,
+                                } = ev.kind
+                                {
+                                    match message {
+                                        MidiMessage::NoteOff { key: key2, .. } if key2 == key => {
+                                            break 'block clock2 - clock;
+                                        }
+                                        // Tricky, but NoteOn with velocity of 0 also means note off, apparently.
+                                        MidiMessage::NoteOn { vel, key: key2 }
+                                            if key2 == key && vel == 0 =>
+                                        {
+                                            break 'block clock2 - clock;
+                                        }
+                                        _ => (),
+                                    }
+                                }
+                            }
+                            panic!("Couldn't determine note duration");
+                        };
+                        // TODO: For some reason some notes play extremely long, but I can't figure out why
+                        if duration > 80_000 {
+                            eprintln!("HACK: Shortening note with way too long duration.");
+                            duration = 100;
+                        }
+                        song.events.eves.push(Event {
+                            payload: EventPayload::On { duration },
+                            unit: UnitIdx(track_idx as u8),
+                            tick: clock,
+                        });
+                    }
+                    MidiMessage::ProgramChange { program } => {
+                        let len = used_programs.len();
+                        let idx = used_programs.entry(program).or_insert(len as u32);
+                        eprintln!("Instrument change of {track_idx} to {program}");
+                        song.events.eves.push(Event {
+                            payload: EventPayload::SetVoice(*idx),
+                            unit: UnitIdx(track_idx as u8),
+                            tick: clock,
+                        });
+                    }
+                    MidiMessage::PitchBend { bend } => {
+                        pitch_bend = bend.as_f64();
+                        if let Some(last) = last_key {
+                            push_key_event(song, base_key, track_idx, clock, pitch_bend, last);
+                        }
+                    }
+                    MidiMessage::Controller { controller, value } => {
+                        match controller.as_int() {
+                            // 7: "Channel volume"
+                            // 11: "Expression" or secondary volume controller
+                            7 | 11 => {
+                                song.events.eves.push(Event {
+                                    payload: EventPayload::Volume(value.as_int() as i16),
+                                    unit: UnitIdx(track_idx as u8),
+                                    tick: clock,
+                                });
+                            }
+                            _ => {
+                                eprintln!("c {controller} = {value}");
+                            }
+                        }
+                    }
+                    _ => eprintln!("Unhandled mid msg: {message:?}"),
+                },
+                TrackEventKind::Meta(meta_message) => match meta_message {
+                    MetaMessage::TrackName(name_bytes) => {
+                        eprintln!("Track name: {}", std::str::from_utf8(name_bytes).unwrap());
+                    }
+                    MetaMessage::EndOfTrack => {}
+                    MetaMessage::Tempo(u24) => {
+                        let ratio = max_tempo as f64 / u24.as_int() as f64;
+                        clock_mul = ratio;
+                    }
+                    MetaMessage::TimeSignature(num, denom, cpt, npq_32nd) => {
+                        eprintln!("Time sig: {num} {denom} {cpt} {npq_32nd}");
+                    }
+                    _ => eprintln!("UNhandled meta: {meta_message:?}"),
+                },
+                _ => eprintln!("Unhandled event kind: {:?}", event.kind),
+            }
+            clock += (event.delta.as_int() as f64 * clock_mul) as u32;
+        }
+        max_clock = max_clock.max(clock);
+    }
+    // TODO: Might not be correct, also individual tracks might have their own tempo
+    // TODO: Magic number pulled out of thin air
+    song.master.timing.bpm = ms_per_beat_to_bpm(max_tempo);
+    song.master.loop_points.last = max_clock
+        / (song.master.timing.ticks_per_beat as u32 * song.master.timing.beats_per_meas as u32);
+
+    // PxTone events seem to need to be stored in order of increasing clock value
+    song.events.eves.sort_by_key(|ev| ev.tick);
+    Output { used_programs }
+}
+
+fn push_key_event(
+    song: &mut Song,
+    base_key: u8,
+    track_idx: usize,
+    clock: u32,
+    pitch_bend: f64,
+    key: u7,
+) {
+    let raw_key = (key.as_int() + base_key) as i32 * 256;
+    // TODO: 2560 magic number, based on ear (and it being 10 times 256, something to do with cents?)
+    let bend_mod = pitch_bend * 2560.0;
+    if bend_mod != 0.0 {
+        song.events.eves.push(Event {
+            payload: EventPayload::PtcowDebug(bend_mod as i32),
+            unit: UnitIdx(track_idx as u8),
+            tick: clock,
+        });
+    }
+    song.events.eves.push(Event {
+        payload: EventPayload::Key((raw_key as f64 + bend_mod) as i32),
+        unit: UnitIdx(track_idx as u8),
+        tick: clock,
+    });
+}
+
+/// Microseconds per minute
+const MS_PER_MINUTE: u32 = 60_000_000;
+
+fn ms_per_beat_to_bpm(ms_per_beat: u32) -> f32 {
+    MS_PER_MINUTE as f32 / ms_per_beat as f32
+}
